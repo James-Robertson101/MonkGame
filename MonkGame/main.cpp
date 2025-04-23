ee4e73c0c601b08e5632280d52d7e43507b3f2f9
James Robertson <jamesrobertsonmcfc@gmail.com>
2025-04-23 07:31:41 +0100

    taken monk parameters in main file

--- MonkGame/main.cpp
+++ MonkGame/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 #include "Dungeon.h"
+
 int main() {
     // Create a Monk instance
-    Monk monk("Hero", "A brave monk seeking adventure!");
+    std::string monkName;
+    std::string monkDescription;
+
+    std::cout << "Enter your Monk's name: ";
+    std::getline(std::cin, monkName);
+
+    std::cout << "Enter a description for your Monk: ";
+    std::getline(std::cin, monkDescription);
+    
+    Monk monk(monkName, monkDescription);
 
     // Generate a dungeon and explore
     Dungeon dungeon;
